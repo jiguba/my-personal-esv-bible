e45a4c607440e895c3a47336099d630eecc25cbd
Jason Guba <jguba@automatapracdev.com>
2026-06-17 15:19:17 -0500

    initial project files

---
+++ src/main/java/dev/jasonguba/esvbible/EsvBibleAppApplication.java
@@ -0,0 +1,13 @@
+package dev.jasonguba.esvbible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsvBibleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsvBibleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jasonguba/esvbible/EsvBibleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jasonguba.esvbible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsvBibleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
