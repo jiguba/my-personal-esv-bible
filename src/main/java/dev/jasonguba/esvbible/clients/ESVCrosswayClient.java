4e3f682ec6c7ddc6caa2ada534b0148d726530b7
Jason Guba <jguba@automatapracdev.com>
2026-06-17 21:43:05 -0500

    Test ESV Crossway API

--- src/main/java/dev/jasonguba/esvbible/clients/ESVCrosswayClient.java
+++ src/main/java/dev/jasonguba/esvbible/clients/ESVCrosswayClient.java
@@ -1,5 +1,6 @@
 package dev.jasonguba.esvbible.clients;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -9,11 +10,14 @@ public class ESVCrosswayClient {
     private static final String PASSAGE_PATH = "/v3/passage/text/";
 
     private final RestClient restClient;
+    private final String apiKey;
 
-    public ESVCrosswayClient(RestClient.Builder restClientBuilder) {
+    public ESVCrosswayClient(RestClient.Builder restClientBuilder, @Value("${esvbible.api.key}") String apiKey) {
         this.restClient = restClientBuilder
             .baseUrl(BASE_URL)
             .build();
+
+        this.apiKey = apiKey;
     }
 
     public String getPassageText() {
@@ -21,7 +25,9 @@ public class ESVCrosswayClient {
             .get()
             .uri(uriBuilder -> uriBuilder
                 .path(PASSAGE_PATH)
+                .queryParam("q","John+3:16")
                 .build())
+            .header("Authorization", apiKey)
             .retrieve()
             .body(String.class);
     }
